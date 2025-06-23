d1840d7f9b976c438ed6b96233cc3e02df85ff4d
tratchapong <tratchapong@gmail.com>
2025-06-23 21:44:40 +0700

    AppRouter with children, Outlet

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,19 +1,28 @@
-import {createBrowserRouter, Navigate, Outlet, RouterProvider} from 'react-router'
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router'
 
 const guestRouter = createBrowserRouter([
-  {path : '/', element: <p>Login</p>},
-  {path : '*', element: <Navigate to='/' />},
+  { path: '/', element: <p>Login</p> },
+  { path: '*', element: <Navigate to='/' /> },
 ])
 
 const userRouter = createBrowserRouter([
-  {path: '/', element: <p>Home</p>},
-  {path: '/friends', element: <p>Friends Home</p>},
-  {path : '*', element: <Navigate to='/' />},
+  {
+    path: '/', element: <>
+      <p className='py-4 border'>Header</p>
+      <Outlet />
+    </>,
+    children: [
+      { path: '', element: <p>User Home : sidebar, posts</p> },
+      { path: 'friends', element: <p>Friends Home</p> },
+      { path: 'profile', element: <p>Profile Home</p> },
+      { path: '*', element: <Navigate to='/' /> },
+    ]
+  }
 ])
 
 function AppRouter() {
   return (
-    <RouterProvider router={userRouter}/>
+    <RouterProvider router={userRouter} />
   )
 }
 
